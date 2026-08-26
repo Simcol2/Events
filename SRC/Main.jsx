9b82ed5e032c5f9f3b6838e084cf2f6c39df2c74
Simcol2 <155248502+Simcol2@users.noreply.github.com>
2026-08-25 23:12:12 -0400

    Set up React application with RentalBooking component

--- SRC/Main.jsx
+++ SRC/Main.jsx
@@ -1,1 +1,10 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import RentalBooking from "../rental-kit-booking-v5.jsx";
+import "./index.css";
 
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RentalBooking />
+  </React.StrictMode>
+);
