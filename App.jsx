3c83b231b11cdd69e0d0e8f4dc6a98fbea5f3b1c
Simcol2 <155248502+Simcol2@users.noreply.github.com>
2026-08-26 23:01:18 -0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+
+import Home from "./pages/Home";
+import Decor from "./pages/Decor";
+import Activities from "./pages/Activities";
+import Collections from "./pages/Collections";
+import HowItWorks from "./pages/HowItWorks";
+import PackageBuilder from "./pages/PackageBuilder";
+
+function getPath() {
+  return window.location.pathname || "/";
+}
+
+export default function App() {
+  const [path, setPath] = useState(getPath);
+
+  useEffect(() => {
+    const handlePopState = () => setPath(getPath());
+
+    window.addEventListener("popstate", handlePopState);
+
+    return () => {
+      window.removeEventListener("popstate", handlePopState);
+    };
+  }, []);
+
+  const navigate = (to) => {
+    if (!to) return;
+
+    window.history.pushState({}, "", to);
+    setPath(to);
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  // Keep navigation compatible with the existing page components.
+  const page = path.split("?")[0].replace(/\/+$/, "") || "/";
+
+  switch (page) {
+    case "/":
+      return <Home navigate={navigate} />;
+
+    case "/decor":
+      return <Decor navigate={navigate} />;
+
+    case "/activities":
+      return <Activities navigate={navigate} />;
+
+    case "/collections":
+      return <Collections navigate={navigate} />;
+
+    case "/how-it-works":
+      return <HowItWorks navigate={navigate} />;
+
+    case "/package-builder":
+      return <PackageBuilder navigate={navigate} />;
+
+    default:
+      return <Home navigate={navigate} />;
+  }
+}
